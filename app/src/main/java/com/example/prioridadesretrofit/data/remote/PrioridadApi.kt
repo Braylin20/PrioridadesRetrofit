4159fe90c882d6e68b12f306644b5986e32a140a
Braylin <braylinvasquez20@gmail.com>
2024-10-06 09:09:36 -0400

    creando la interfaz PrioridadApi

--- app/src/main/java/com/example/prioridadesretrofit/data/remote/PrioridadApi.kt
+++ app/src/main/java/com/example/prioridadesretrofit/data/remote/PrioridadApi.kt
@@ -1,4 +1,16 @@
 package com.example.prioridadesretrofit.data.remote
 
+import com.example.prioridadesretrofit.data.remote.dto.PrioridadDto
+import retrofit2.http.Body
+import retrofit2.http.GET
+import retrofit2.http.POST
+import retrofit2.http.Path
+
 interface PrioridadApi {
+    @GET("api/Prioridades/{id}")
+    suspend fun getPrioriad(@Path("id") id: Int): PrioridadDto
+    @GET("api/Prioridades")
+    suspend fun getPrioriades(): List<PrioridadDto>
+    @POST
+    suspend fun addPrioridad(@Body prioridad: PrioridadDto): PrioridadDto
 }
