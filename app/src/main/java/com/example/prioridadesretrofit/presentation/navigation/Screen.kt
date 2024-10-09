106971edf311f8ecc2311d8ecb0d0d55d5e66f3d
Braylin <braylinvasquez20@gmail.com>
2024-10-08 21:14:24 -0400

    Agregando Screen de cliente, sistema y ticket

--- app/src/main/java/com/example/prioridadesretrofit/presentation/navigation/Screen.kt
+++ app/src/main/java/com/example/prioridadesretrofit/presentation/navigation/Screen.kt
@@ -9,4 +9,16 @@ sealed class Screen {
     data object PrioridadList : Screen()
     @Serializable
     data class Prioridad(val prioridadId: Int) : Screen()
+    @Serializable
+    data object TicketList: Screen()
+    @Serializable
+    data class Ticket(val ticketId: Int): Screen()
+    @Serializable
+    data object SistemaList: Screen()
+    @Serializable
+    data class Sistema(val sistemaId: Int): Screen()
+    @Serializable
+    data object ClienteList: Screen()
+    @Serializable
+    data class Cliente(val clienteId: Int): Screen()
 }
